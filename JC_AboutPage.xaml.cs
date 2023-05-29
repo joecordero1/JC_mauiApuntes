cc17a5c221eec6438993aad3f41d3e137d3c38ac
Joe Mateo Cordero Flores <joe.cordero@udla.edu.ec>
2023-05-29 08:17:40 -0500

    Se modificó el AppShell.xaml

--- JC_AboutPage.xaml.cs
+++ JC_AboutPage.xaml.cs
@@ -6,4 +6,10 @@ public partial class JC_AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void LearnMore_Clicked(object sender, EventArgs e)
+    {
+        // Navigate to the specified URL in the system browser.
+        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+    }
 }
