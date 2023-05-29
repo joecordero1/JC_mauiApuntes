1411890e3a97daadf6558c1baba23f894571db29
Joe Mateo Cordero Flores <joe.cordero@udla.edu.ec>
2023-05-29 08:48:24 -0500

    Se modificó el AppShell y se creó la NotePage

---
+++ JC_NotePage.xaml.cs
@@ -0,0 +1,26 @@
+namespace JC_mauiApuntes;
+
+public partial class JC_NotePage : ContentPage
+{
+    string _fileName = Path.Combine(FileSystem.AppDataDirectory, "JC_BaseApuntes.txt");
+    public JC_NotePage()
+	{
+		InitializeComponent();
+        if (File.Exists(_fileName))
+            JC_TextEditor.Text = File.ReadAllText(_fileName);
+    }
+    private void SaveButton_Clicked(object sender, EventArgs e)
+    {
+        // Save the file.
+        File.WriteAllText(_fileName, JC_TextEditor.Text);
+    }
+
+    private void DeleteButton_Clicked(object sender, EventArgs e)
+    {
+        // Delete the file.
+        if (File.Exists(_fileName))
+            File.Delete(_fileName);
+
+        JC_TextEditor.Text = string.Empty;
+    }
+}
