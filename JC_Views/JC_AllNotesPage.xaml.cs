6eff44fc719f315e263d21c7dda3c449f14de99f
Joe Mateo Cordero Flores <joe.cordero@udla.edu.ec>
2023-05-29 23:37:15 -0500

    Se modificó el arbol visual

---
+++ JC_Views/JC_AllNotesPage.xaml.cs
@@ -0,0 +1,36 @@
+namespace JC_mauiApuntes.JC_Views;
+
+public partial class JC_AllNotesPage : ContentPage
+{
+    public JC_AllNotesPage()
+    {
+        InitializeComponent();
+
+        BindingContext = new JC_Models.AllNotes();
+    }
+
+    protected override void OnAppearing()
+    {
+        ((JC_Models.AllNotes)BindingContext).LoadNotes();
+    }
+
+    private async void Add_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync(nameof(NotePage));
+    }
+
+    private async void notesCollection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.Count != 0)
+        {
+            // Get the note model
+            var note = (JC_Models.JC_Note)e.CurrentSelection[0];
+
+            // Should navigate to "NotePage?ItemId=path\on\device\XYZ.notes.txt"
+            await Shell.Current.GoToAsync($"{nameof(NotePage)}?{nameof(NotePage.ItemId)}={note.Filename}");
+
+            // Unselect the UI
+            notesCollection.SelectedItem = null;
+        }
+    }
+}
--- JC_Views/JC_NotePage.xaml.cs
+++ JC_Views/JC_NotePage.xaml.cs
@@ -1,26 +1,53 @@
 namespace JC_mauiApuntes.JC_Views;
 
+[QueryProperty(nameof(ItemId), nameof(ItemId))]
 public partial class JC_NotePage : ContentPage
 {
     string _fileName = Path.Combine(FileSystem.AppDataDirectory, "JC_BaseApuntes.txt");
+    public string ItemId
+    {
+        set { LoadNote(value); }
+    }
+    
     public JC_NotePage()
 	{
 		InitializeComponent();
-        if (File.Exists(_fileName))
-            JC_TextEditor.Text = File.ReadAllText(_fileName);
+        string appDataPath = FileSystem.AppDataDirectory;
+        string randomFileName = $"{Path.GetRandomFileName()}.notes.txt";
+
+        LoadNote(Path.Combine(appDataPath, randomFileName));
     }
-    private void SaveButton_Clicked(object sender, EventArgs e)
+    private async void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // Save the file.
-        File.WriteAllText(_fileName, JC_TextEditor.Text);
+        if (BindingContext is JC_Models.JC_Note note)
+            File.WriteAllText(note.Filename, JC_TextEditor.Text);
+
+        await Shell.Current.GoToAsync("..");
     }
 
-    private void DeleteButton_Clicked(object sender, EventArgs e)
+    private async void DeleteButton_Clicked(object sender, EventArgs e)
     {
-        // Delete the file.
-        if (File.Exists(_fileName))
-            File.Delete(_fileName);
+        if (BindingContext is JC_Models.JC_Note note)
+        {
+            // Delete the file.
+            if (File.Exists(note.Filename))
+                File.Delete(note.Filename);
+        }
 
-        JC_TextEditor.Text = string.Empty;
+        await Shell.Current.GoToAsync("..");
+    }
+
+    private void LoadNote(string fileName)
+    {
+        JC_Models.JC_Note noteModel = new JC_Models.JC_Note();
+        noteModel.Filename = fileName;
+
+        if (File.Exists(fileName))
+        {
+            noteModel.Date = File.GetCreationTime(fileName);
+            noteModel.Text = File.ReadAllText(fileName);
+        }
+
+        BindingContext = noteModel;
     }
 }
